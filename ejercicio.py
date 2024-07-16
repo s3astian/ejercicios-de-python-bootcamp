9944c671afc9b726399aa3ec893e2252a611a32c
johan sebastian diaz delgado <jhoandiaz24@gmail.com>
2024-07-15 19:58:54 -0500

    ejercicio numero uno

---
+++ ejercicio.py
@@ -0,0 +1,22 @@
+# resolucion de primer ejercicio de python por Johan Sebastian Diaz Delgado correo : jhoandiaz24@gmail.com
+# introduccion solicitud de datos con usando print e input...... 
+print("hola a continuacion se presentara el desarrollo del primer ejercicio de Python Bienvenido jse te solicitaran algunos datos que son necesario para el desarrollo de este mismo gracias por tu participacion. ")
+print("\t")
+nombre = input("por favor ingresa tu nombre completo :")
+print("\t")
+edad = input("como segundo dato debo saber tu edad por favor ingresalo aqui :")
+print("\t")
+# precentacion de datos solicitados en pantalla
+print("te doy la bienvenida " + nombre + "a este ejercicio practico,  no sabia que alguien con " + edad + " pudiera verse tan joven" )
+print("\t")
+# solicitud de un numero al azar para realizar una operacion matematica ya antes establecida y conversion de la variable string a entero
+multi =  int (input ("para continuar con el ejercicio por favor ingresa un numero al azar te demostrare algo de mi ingenio matematico :"))
+# realizacion de la operacion
+multiplica = multi * 2
+print("\t")
+# precentacion de datos solicitados en pantalla y muestra de resultados de la operacion
+print ("sabias que el numero que ingresaste ", multi , " multiplicado por 2 da: " , multiplica  )
+print("\t")
+# despedida
+print("esto es todo gracias por brindarme un poco de tu tiempo te espero en el proximo desarrollo de la siguiente aplicasion del proximo ejercicio.")
+print("ᕙ(`▿´)ᕗ")
